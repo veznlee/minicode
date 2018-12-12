61536cfe2e54098d548bfdb498411e498ae138e3
veznlee <veznlee@qq.com>
2018-12-12 18:01:23 +0800

    ''

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,20 @@
 //获取应用实例
 const app = getApp()
 
+/*
+ * wx.canIUse(string)
+ * 使用 ${API}.${method}.${param}.${options} 或者 ${component}.${attribute}.${option} 方式来调用
+ * 
+ * ${API} 代表 API 名字
+   ${method} 代表调用方式，有效值为return, success, object, callback
+   ${param} 代表参数或者返回值
+   ${options} 代表参数的可选值
+   ${component} 代表组件名字
+   ${attribute} 代表组件属性
+   ${option} 代表组件属性的可选值
+ */
+
+
 Page({
   data: {
     motto: 'Hello World',
